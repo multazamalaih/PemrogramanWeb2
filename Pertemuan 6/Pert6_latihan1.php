1dfb90dabb45380cdca1b280dcee0669c104998c
Multazam <67831738+multazamalaih@users.noreply.github.com>
2021-04-03 01:36:59 +0700

    Add files via upload

---
+++ Pertemuan 6/Pert6_latihan1.php
@@ -0,0 +1,22 @@
+<?php
+
+    $a[0] =1;
+    $a[1] =3;
+    $a[2] =5;
+    $jumlah =count($a);
+
+    echo "Jumlah array a = $jumlah <br>";
+
+    // variabel $jumlah akan bernilai 3
+
+    $b["buah"] = "semangka";
+    $b["sayur"] ="wortel";
+    $b["daging"] ="ayam";
+    $b["utama"] ="nasi";
+    $jumlah = sizeof($b);
+    
+    echo "Jumlah array b = $jumlah <br>";
+
+    // variabel $jumlah akan bernilai 4
+
+?> 
---
+++ Pertemuan 6/Pert6_latihan2.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <?php
+
+            $x=array("one","two","three");
+            foreach ($x as $value)
+            {
+
+            echo $value . "<br />";
+            
+            }
+
+        ?>
+    </body>
+</html>
