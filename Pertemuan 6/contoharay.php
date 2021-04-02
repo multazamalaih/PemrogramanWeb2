e8a36a293a948b1e226023e83275d5f7ea7ba5b3
Multazam <67831738+multazamalaih@users.noreply.github.com>
2021-04-03 01:35:57 +0700

    Add files via upload

---
+++ Pertemuan 6/contoharay.php
@@ -0,0 +1,10 @@
+<?Php
+
+$personal["nama"]="Salman";
+$personal["kendaraan"] ="Minibus";
+$personal["rumah"] ="Perumnas";
+echo $personal["nama"] . "\n";
+echo $personal["kendaraan"] . "\n";
+echo $personal["rumah"]."\n";  
+
+?> 
